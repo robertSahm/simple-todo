1b2ef22b1916375b19604e2b0741223d7326cd64
robertSahm <robertsahm@gmail.com>
2019-05-19 23:46:24 -0600

    updated

--- frontend/components/todo-input.js
+++ frontend/components/todo-input.js
@@ -1,41 +1,43 @@
-import React, { memo } from "react";
+import React from "react";
 import { TextField, Paper, Button, Grid } from "@material-ui/core";
 
-const handleChange = e => {
-	this.setState({ newText: e.target.value })
+const addButton = {
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
 }
 
-const handleKeyDown = e => {
-	if (e.keyCode !== ENTER_KEY_CODE) return
-	this.createTodo()
-}
+const Input = props => {
+  return (
+    <Paper style={{ marginLeft: 1, marginRight: 1, marginTop: 16, padding: 16 }}>
+      <Grid container alignItems="center">
+        <Grid xs={10} md={11} item style={{ paddingRight: 16 }}>
+          <TextField
+            autoFocus
+            fullWidth
+            onChange={props.handleChange}
+            onKeyDown={props.handleKeyDown}
+            value={props.newText}
+            align="center"
+            label="Enter todo item"
+          />
+        </Grid>
+        <Grid xs={2} md={1} item>
+          <Button
+            fullWidth
+            style={addButton}
+            color="inherit"
+            onClick={props.createTodo}
+          >
+            Add
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
 
-const Input = () => {
-  <Paper style={{ marginLeft: 1, marginRight: 1, marginTop: 16, padding: 16 }}>
-    <Grid container alignItems="center">
-      <Grid xs={10} md={11} item style={{ paddingRight: 16 }}>
-        <TextField
-          autoFocus
-          fullWidth
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          value={this.state.newText}
-          align="center"
-          label="Enter todo item"
-        />
-      </Grid>
-      <Grid xs={2} md={1} item>
-        <Button
-          fullWidth
-          style={addButton}
-          color="inherit"
-          onClick={this.createTodo}
-        >
-          Add
-        </Button>
-      </Grid>
-    </Grid>
-  </Paper>
+  )
 };
 
 export default Input
